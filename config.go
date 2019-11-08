487a48ef6a11bd3a70e280d76a773e3318c257e1
Alexey Rusov <rolic402@mail.ru>
2019-11-08 23:46:54 +0300

    +Check +Name&Description

--- config.go
+++ config.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"reflect"
 	"regexp"
 	"strings"
 
@@ -17,6 +18,9 @@ import (
 
 // Common --
 type Common struct {
+	Name        string `toml:"name"`
+	Description string `toml:"description"`
+
 	LogLocalTime   bool   `toml:"log-local-time"`
 	LogDir         string `toml:"log-dir"`
 	LogLevel       string `toml:"log-level"`
@@ -162,6 +166,72 @@ func LoadFile(fileName string, cfg interface{}) error {
 // GetText -- get prepared configuration text
 func GetText() []byte {
 	return configText
+}
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+// Check --
+func (x *Common) Check(cfg interface{}) (err error) {
+	var msgs []string
+
+	if x.Name == "" {
+		x.Name = misc.AppName()
+	}
+
+	return misc.JoinedError(msgs)
+}
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+// Check --
+func Check(cfg interface{}, list []interface{}) error {
+	var msgs []string
+
+	for _, x := range list {
+		v := reflect.ValueOf(x)
+
+		if v.Kind() != reflect.Ptr {
+			misc.AddMessage(&msgs, fmt.Sprintf(`"%#v" is not pointer`, x))
+			continue
+		}
+
+		if v.Elem().Kind() != reflect.Struct {
+			misc.AddMessage(&msgs, fmt.Sprintf(`"%#v" is not pointer to struct`, x))
+			continue
+		}
+
+		m := v.MethodByName("Check")
+
+		if m.Kind() != reflect.Func {
+			misc.AddMessage(&msgs, fmt.Sprintf(`"%#v" doesn't have the "Check" method`, x))
+			continue
+		}
+
+		e := m.Call([]reflect.Value{reflect.ValueOf(cfg)})
+
+		if len(e) != 1 || e[0].Kind() != reflect.Interface {
+			misc.AddMessage(&msgs, fmt.Sprintf(`"%#v" method "Check" returned illegal value`, x))
+			continue
+		}
+
+		if e[0].IsNil() {
+			continue
+		}
+
+		err, ok := e[0].Interface().(error)
+		if !ok {
+			misc.AddMessage(&msgs, fmt.Sprintf(`"%#v" method "Check" returned not error value`, x))
+			continue
+		}
+
+		if err == nil {
+			continue
+		}
+
+		misc.AddMessage(&msgs, err.Error())
+	}
+
+	return misc.JoinedError(msgs)
 }
 
 //----------------------------------------------------------------------------------------------------------------------------//
