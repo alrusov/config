714d3cdc610b6e1b2cc886926df7ccb4ce508185
Alexey Rusov <rolic402@mail.ru>
2020-05-03 12:15:45 +0300

    messages improved

--- check.go
+++ check.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"reflect"
 	"strings"
 
@@ -69,26 +68,26 @@ func Check(cfg interface{}, list []interface{}) error {
 		v := reflect.ValueOf(x)
 
 		if v.Kind() != reflect.Ptr {
-			misc.AddMessage(&msgs, fmt.Sprintf(`"%#v" is not pointer`, x))
+			misc.AddMessage(&msgs, `"%#v" is not a pointer`, x)
 			continue
 		}
 
 		//if v.Elem().Kind() != reflect.Struct {
-		//	misc.AddMessage(&msgs, fmt.Sprintf(`"%#v" is not pointer to struct`, x))
+		//	misc.AddMessage(&msgs, `"%#v" is not a pointer to struct`, x)
 		//	continue
 		//}
 
 		m := v.MethodByName("Check")
 
 		if m.Kind() != reflect.Func {
-			misc.AddMessage(&msgs, fmt.Sprintf(`"%#v" doesn't have the "Check" method`, x))
+			misc.AddMessage(&msgs, `"%#v" doesn't have the "Check" method`, x)
 			continue
 		}
 
 		e := m.Call([]reflect.Value{reflect.ValueOf(cfg)})
 
 		if len(e) != 1 || e[0].Kind() != reflect.Interface {
-			misc.AddMessage(&msgs, fmt.Sprintf(`"%#v" method "Check" returned illegal value`, x))
+			misc.AddMessage(&msgs, `"%#v" method "Check" returned illegal value`, x)
 			continue
 		}
 
@@ -98,7 +97,7 @@ func Check(cfg interface{}, list []interface{}) error {
 
 		err, ok := e[0].Interface().(error)
 		if !ok {
-			misc.AddMessage(&msgs, fmt.Sprintf(`"%#v" method "Check" returned not error value`, x))
+			misc.AddMessage(&msgs, `"%#v" method "Check" returned not error value`, x)
 			continue
 		}
 
@@ -106,7 +105,7 @@ func Check(cfg interface{}, list []interface{}) error {
 			continue
 		}
 
-		misc.AddMessage(&msgs, err.Error())
+		misc.AddMessage(&msgs, "%s", err.Error())
 	}
 
 	return misc.JoinedError(msgs)
--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
 module github.com/alrusov/config
 
-go 1.13
+go 1.14
 
 require (
 	github.com/alrusov/bufpool v0.1.2
-	github.com/alrusov/misc v0.1.13
+	github.com/alrusov/misc v0.1.14
 	github.com/naoina/go-stringutil v0.1.0 // indirect
 	github.com/naoina/toml v0.1.1
 )
