17522915af6bbfad526061fca6709fed15364ea4
Alexey Rusov <rolic402@mail.ru>
2023-03-22 11:04:51 +0300

    Duration.D()

--- duration.go
+++ duration.go
@@ -17,7 +17,6 @@ type (
 // UnmarshalText implements encoding.TextUnmarshaler
 func (d *Duration) UnmarshalText(data []byte) error {
 	duration, err := misc.Interval2Duration(string(data))
-	//duration, err := time.ParseDuration(string(data))
 	if err == nil {
 		*d = Duration(duration)
 	}
@@ -26,8 +25,13 @@ func (d *Duration) UnmarshalText(data []byte) error {
 
 // MarshalText implements encoding.TextMarshaler
 func (d Duration) MarshalText() ([]byte, error) {
-	//return []byte(time.Duration(d).String()), nil
 	return []byte(misc.Int2Interval(int64(d))), nil
+}
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+func (d Duration) D() time.Duration {
+	return time.Duration(d)
 }
 
 //----------------------------------------------------------------------------------------------------------------------------//
